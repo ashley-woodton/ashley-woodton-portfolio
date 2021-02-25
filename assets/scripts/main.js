eab36955efe8d66750d19409736be4a3da094cc8
Ashley Woodton <ashleywoodton@MacBook-Pro-2.attlocal.net>
2021-02-25 00:02:15 -0500

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,42 @@
+//Mobile Menu
+const navMenu = document.getElementById('menu');
+const toggleMenu = document.getElementById('nav-toggle');
+const closeMenu = document.getElementById('nav-close');
+
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.add('show');
+})
+
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show');
+})
+
+
+const navLink = document.querySelectorAll('.nav_link')
+
+
+navLink.forEach(n=>n.addEventListener('click', ()=>{
+    navMenu.classList.remove('show');
+}))
+
+//section scroll
+const section = document.querySelectorAll('section[i]');
+window.addEventListener('scroll', scrollActive)
+
+activateScroll = () => {
+    const scroll = window.pageYOffset;
+
+    section.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionID = current.getAttribute('id');
+
+        if(scroll > sectionTop && scroll <= sectionTop + sectionHeight){
+            document.querySelector(`.menu a[href*= ${sectionID}`).classList.add('active');
+        }else{
+            document.querySelector(`.menu a[href*= ${sectionID}`).classList.remove ('active');
+        }
+    })
+
+    
+}
